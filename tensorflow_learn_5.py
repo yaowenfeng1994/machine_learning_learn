b4529a295416f20cefb3f96bcfa06ddb2aae063e
bee <610733719@qq.com>
2019-02-13 18:01:51 +0800

    update

---
+++ tensorflow_learn_5.py
@@ -0,0 +1,45 @@
+#! -*- coding:utf-8 -*-
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import os
+import struct
+
+# one_hot 只有0跟1
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+batch_size = 100
+n_batch = mnist.train.num_examples // batch_size
+print(n_batch)
+
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, 10])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+prediction = tf.nn.softmax(tf.matmul(x, W) + b)
+
+loss = tf.reduce_mean(tf.square(y - prediction))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+init = tf.global_variables_initializer()
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(prediction, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+filename = 'train-labels-idx1-ubyte'
+binfile = open(filename, 'rb')
+buf = binfile.read()
+print(buf)
+index = 0
+struct.unpack_from('>IIII', buf, index)
+index += struct.calcsize('>IIII')
+im = struct.unpack_from('>784B', buf, index)
+index += struct.calcsize('>784B')
+im = np.array(im)
+im = im.reshape(28, 28)
+fig = plt.figure()
+plotwindow = fig.add_subplot(111)
+plt.imshow(im, cmap='gray')
+plt.show()
