1900fc3901385a086098cab6812db673b76e22b1
bee <610733719@qq.com>
2019-02-14 15:38:34 +0800

    fix

--- tensorflow_learn_5.py
+++ tensorflow_learn_5.py
@@ -20,7 +20,8 @@ W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 prediction = tf.nn.softmax(tf.matmul(x, W) + b)
 
-loss = tf.reduce_mean(tf.square(y - prediction))
+# loss = tf.reduce_mean(tf.square(y - prediction))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction))
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.global_variables_initializer()
@@ -29,7 +30,7 @@ correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(prediction, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 with tf.Session() as sess:
     sess.run(init)
-    for epoch in range(51):
+    for epoch in range(21):
         for batch in range(n_batch):
             images, labels = mnist.train.next_batch(batch_size)
             sess.run(train_step, feed_dict={x: images, y: labels})
