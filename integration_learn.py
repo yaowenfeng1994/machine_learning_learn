868f9413e09cd56d47b3547b1eba6e824372c53c
twist <twist.yao@klook.com>
2019-12-24 09:37:04 +0800

    update

--- integration_learn.py
+++ integration_learn.py
@@ -1,10 +1,12 @@
+import warnings
+
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
-from sklearn.preprocessing import LabelEncoder
-from sklearn.externals import six
 from sklearn.base import clone
+from sklearn.externals import six
 from sklearn.pipeline import _name_estimators
-import numpy as np
+
+warnings.filterwarnings('ignore')  # 忽略warning
 
 
 class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
@@ -26,6 +28,9 @@ class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
         self.named_classifiers = {key: value for key, value in _name_estimators(classifiers)}
         self.vote = vote
         self.weights = weights
+        self.lablenc_ = object
+        self.classes_ = object
+        self.classifiers_ = []
 
     def fit(self, X, y):
         '''
@@ -88,3 +93,60 @@ class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
                 for key, value in six.iteritems(step.get_params(deep=True)):
                     out['%s__%s' % (name, key)] = value
             return out
+
+
+if __name__ == "__main__":
+    from sklearn import datasets
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.pipeline import Pipeline
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.model_selection import cross_val_score
+    import numpy as np
+
+    iris = datasets.load_iris()
+    x, y = iris.data[50:, [1, 2]], iris.target[50:]
+    le = LabelEncoder()
+    y = le.fit_transform(y)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5, random_state=1)
+    clf1 = LogisticRegression(penalty="l2", C=0.001, random_state=0)
+    clf2 = DecisionTreeClassifier(max_depth=1, criterion="entropy", random_state=0)
+    clf3 = KNeighborsClassifier(n_neighbors=1, p=2, metric="minkowski")
+    pipe1 = Pipeline([["sc", StandardScaler()], ["clf", clf1]])
+    pipe3 = Pipeline([["sc", StandardScaler()], ["clf", clf3]])
+    clf_labels = ["Logistic Regression", "Decision Tree", "KNN"]
+    # for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
+    #     scores = cross_val_score(estimator=clf, X=x_train, y=y_train, cv=10, scoring="roc_auc")
+    #     print("ROC AUC: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+
+    mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
+    clf_labels += ["Majority Voting"]
+    all_clf = [pipe1, clf2, pipe3, mv_clf]
+    for clf, label in zip(all_clf, clf_labels):
+        scores = cross_val_score(estimator=clf, X=x_train, y=y_train, cv=10, scoring="roc_auc")
+        print("ROC AUC: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+
+    ####################################################################################################################
+
+    from sklearn.metrics import roc_curve, auc
+    import matplotlib.pyplot as plt
+
+    colors = ["black", "orange", "blue", "green"]
+    line_styles = [":", "--", "-.", "-"]
+    for clf, label, clr, ls in zip(all_clf, clf_labels, colors, line_styles):
+        y_pred = clf.fit(x_train, y_train).predict_proba(x_test)[:, 1]
+        fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+        roc_auc = auc(x=fpr, y=tpr)
+        plt.plot(fpr, tpr, color=clr, linestyle=ls, label="%s (auc=%0.2f)" % (label, roc_auc))
+
+    plt.legend(loc="lower right")
+    plt.plot([0, 1], [0, 1], linestyle="--", color="gray", linewidth=2)
+    plt.xlim([-0.1, 1.1])
+    plt.ylim([-0.1, 1.1])
+    plt.grid()
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.show()
