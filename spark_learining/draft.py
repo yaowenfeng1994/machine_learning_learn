b40a067559f0e895a38d6a77e2480fc64ba6964a
twist <twist.yao@klook.com>
2020-01-26 19:44:30 +0800

    准备hadoop， spark环境
    spark客户端向spark服务端master提交任务，由worker向hadoop读取数据，hadoop也是分name node跟data node，向worker返回data node地址

--- spark_learining/draft.py
+++ spark_learining/draft.py
@@ -1,4 +1,25 @@
-import os
-import sys
 from pyspark import SparkContext
-sc = SparkContext("local", "First App")
+
+sc = SparkContext("spark://node-1:7077", "count app")
+# logData = sc.textFile("file:///home/w.txt").cache()
+logData = sc.textFile("hdfs://node-hadoop:9000/hello").cache()
+
+# words = sc.parallelize(
+#     ["scala",
+#      "java",
+#      "hadoop",
+#      "spark",
+#      "akka",
+#      "spark vs hadoop",
+#      "pyspark",
+#      "pyspark and spark"
+#      ])
+# counts = words.count()
+# print("Number of elements in RDD -> %i" % counts)
+
+resultRdd = logData.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+resultColl = resultRdd.collect()
+for line in resultColl:
+    print(line)
+
+sc.stop()
